d661f78d81bccbfec08ef7d6b47b8d75b65b9dde
Pengbowei <cfn55892@gmail.com>
2021-01-15 15:46:08 +0800

    unit5

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+(RESTART)
+@SCREEN
+D=A 
+@0
+M=D	//將SCREEN起始位置存入RAM[0]
+
+///////////////////////////
+(KBDCHECK)
+
+@KBD
+D=M
+@BLACK
+D;JGT	//如果按下KBD任何鍵JUMP TO BLACK
+@WHITE
+D;JEQ	//否則 TIMP TO WHITE
+
+@KBDCHECK
+0;JMP
+///////////////////////////
+(BLACK)
+@1
+M=-1	//螢幕反黑
+@CHANGE
+0;JMP
+
+(WHITE)
+@1
+M=0	//螢幕反白
+@CHANGE
+0;JMP
+//////////////////////////
+(CHANGE)
+@1	//檢查螢幕式黑色還是白色
+D=M	//暫存器保存現在螢幕的值
+
+@0
+A=M	//取得螢幕pixel位置
+M=D	//反黑或反白
+
+@0
+D=M+1	//INC TO NEXT PIXEL
+@KBD
+D=A-D	//KBD-SCREEN=A
+
+@0
+M=M+1	//INC TO NEXT PIXEL
+A=M
+
+@CHANGE
+D;JGT	//IF A=0 EXIT AS THE WHOLE SCREEN IS BLACK
+/////////////////////////
+@RESTART
+0;JMP
